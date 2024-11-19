82af7a6ef58c56efe9f12dca3f41afbc5b294237
Noelle Aly <noellekaly@gmail.com>
2024-11-19 09:25:26 +0000

    Prettier formatting

--- src/components/Dedupe/Dedupe.tsx
+++ src/components/Dedupe/Dedupe.tsx
@@ -1,35 +1,37 @@
-import { Component } from 'react';
-import React from 'react';
-import Accordion from 'react-bootstrap/Accordion';
-import AccordionHeader from 'react-bootstrap/esm/AccordionHeader';
+import { Component } from "react";
+import React from "react";
+import Accordion from "react-bootstrap/Accordion";
+import AccordionHeader from "react-bootstrap/esm/AccordionHeader";
 
 class Dedupe extends Component {
 
-  displaySequences() { 
-    return this.props.sequences.map(element => {
-      return <div>
-        {element}
-      </div>
+  displaySequences() {
+    return this.props.sequences.map((element) => {
+      return <div>{element}</div>;
     });
   }
 
   findUniques() {
     let uniqueSeq = {};
-    this.props.sequences.map(seq => {
-      uniqueSeq[seq] ? uniqueSeq[seq] += 1 : uniqueSeq[seq] = 1
+    this.props.sequences.map((seq) => {
+      uniqueSeq[seq] ? (uniqueSeq[seq] += 1) : (uniqueSeq[seq] = 1);
     });
-    return Object.keys(uniqueSeq).map(seq => {
-      return <div>{seq}: {uniqueSeq[seq]} Occurrence(s)</div>
-    })
+    return Object.keys(uniqueSeq).map((seq) => {
+      return (
+        <div>
+          {seq}: {uniqueSeq[seq]} Occurrence(s)
+        </div>
+      );
+    });
   }
 
   findBaseDifferences(str1: string, str2: string) {
     const maxLength = Math.max(str1.length, str2.length);
     const diff: string[] = [];
 
-    Array.from({length: maxLength}).forEach((_, i)=>{
+    Array.from({ length: maxLength }).forEach((_, i) => {
       if (str1[i] !== str2[i]) {
-        diff.push(`Base ${i}: '${str1[i] || ' '} vs '${str2[i] || ' '}`);
+        diff.push(`Base ${i}: '${str1[i] || " "} vs '${str2[i] || " "}`);
       }
     });
     return diff;
@@ -37,90 +39,72 @@ class Dedupe extends Component {
 
   findSeqWithThresh(threshold?) {
     let uniqueSeq = {};
-    this.props.sequences.map(seq => {
+    this.props.sequences.map((seq) => {
       if (uniqueSeq[seq]) {
         uniqueSeq[seq] += 1;
-      }
-      else if (!uniqueSeq[seq] && Object.values(uniqueSeq).length != 0) {
-        (this.findBaseDifferences(seq, Object.keys(uniqueSeq)[0])).length <= threshold ? uniqueSeq[Object.keys(uniqueSeq)[0]] += 1: uniqueSeq[seq] = 1;
-    }
-      else uniqueSeq[seq] = 1;
-    })
-    return Object.keys(uniqueSeq).map(seq => {
-      return <div>{seq}: {uniqueSeq[seq]} Occurrence(s)</div>
-    })
+      } else if (!uniqueSeq[seq] && Object.values(uniqueSeq).length != 0) {
+        this.findBaseDifferences(seq, Object.keys(uniqueSeq)[0]).length <=
+        threshold
+          ? (uniqueSeq[Object.keys(uniqueSeq)[0]] += 1)
+          : (uniqueSeq[seq] = 1);
+      } else uniqueSeq[seq] = 1;
+    });
+    return Object.keys(uniqueSeq).map((seq) => {
+      return (
+        <div>
+          {seq}: {uniqueSeq[seq]} Occurrence(s)
+        </div>
+      );
+    });
   }
 
   deduplicate(threshold?) {
     if (!threshold) {
-      return this.findUniques() 
-    }
-    else {
+      return this.findUniques();
+    } else {
       return this.findSeqWithThresh(threshold);
     }
-}
-  
-  render() { 
+  }
+
+  render() {
     return (
       <div className="Dedupe">
         Deduplication
         <Accordion>
           <Accordion.Item eventKey="0">
-            <AccordionHeader>
-              Raw Sequences:
-            </AccordionHeader>
+            <AccordionHeader>Raw Sequences:</AccordionHeader>
             <Accordion.Body>
-              <div style={{color: 'black'}}>
-                {this.displaySequences()}
-              </div>
+              <div style={{ color: "black" }}>{this.displaySequences()}</div>
             </Accordion.Body>
-           </Accordion.Item>
+          </Accordion.Item>
         </Accordion>
-
-
         <Accordion>
           <Accordion.Item eventKey="0">
-            <AccordionHeader>
-              Unique Sequences:
-            </AccordionHeader>
+            <AccordionHeader>Unique Sequences:</AccordionHeader>
             <Accordion.Body>
-              <div style={{color: 'black'}}>
-                {this.deduplicate()}
-              </div>
+              <div style={{ color: "black" }}>{this.deduplicate()}</div>
             </Accordion.Body>
-           </Accordion.Item>
+          </Accordion.Item>
         </Accordion>
-
         <Accordion>
           <Accordion.Item eventKey="0">
-            <AccordionHeader>
-              Matching Sequences (Threshold=1):
-            </AccordionHeader>
+            <AccordionHeader>Matching Sequences (Threshold=1):</AccordionHeader>
             <Accordion.Body>
-              <div style={{color: 'black'}}>
-                {this.deduplicate(1)}
-              </div>
+              <div style={{ color: "black" }}>{this.deduplicate(1)}</div>
             </Accordion.Body>
-           </Accordion.Item>
+          </Accordion.Item>
         </Accordion>
-
         <Accordion>
           <Accordion.Item eventKey="0">
-            <AccordionHeader>
-              Matching Sequences (Threshold=2):
-            </AccordionHeader>
+            <AccordionHeader>Matching Sequences (Threshold=2):</AccordionHeader>
             <Accordion.Body>
-              <div style={{color: 'black'}}>
-                {this.deduplicate(2)}
-              </div>
+              <div style={{ color: "black" }}>{this.deduplicate(2)}</div>
             </Accordion.Body>
-           </Accordion.Item>
+          </Accordion.Item>
         </Accordion>
-
-    </div>
-    )
+      </div>
+    );
   }
 }
 
 export default Dedupe;
-
